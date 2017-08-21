64b14b6d84d294f828dfda73db725c8395f8ded1
JJo32 <joshuajoseph07@gmail.com>
2017-08-20 23:58:38 -0400

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -37,4 +37,4 @@
         
         png(filename = "plot1.png", width = 480, height = 480)
         plot(plot1,col = "red", main = "Global Active Power", xlab = "Global Active Power (kilowatts)")
-dev.off()
+        dev.off()
